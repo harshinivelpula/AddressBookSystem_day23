88121ed00f30d9eeb7f7c76a45dcfe85be2fc827
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-11-13 15:29:28 +0530

    add contacts

---
+++ Contact.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem3
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string city { get; set; }
+        public string State { get; set; }
+        public string email { get; set; }
+        public int Zip { get; set; }
+        public int phone { get; set; }
+
+    }
+    public class AddressBook
+    {
+        public void Contact()
+        {
+            List<Contact> contactList = new List<Contact>();
+            contactList.Add(new Contact
+            {
+                Name = "charu",
+                Address = "India",
+                city = "Chennai",
+                State = "tml",
+                email = "charu@gmail.com",
+                Zip = 987654,
+                phone = 987654321
+            });
+
+            contactList.Add(new Contact
+            {
+                Name = "chocky",
+                Address = "india",
+                city = "bihar",
+                State = "br",
+                email = "chocky@gmail.com",
+                Zip = 876543,
+                phone = 1225461458
+            });
+            contactList.Add(new Contact
+            {
+                Name = "haswika",
+                Address = "india",
+                city = "bengaluru",
+                State = "krn",
+                email = "haswika@gmail.com",
+                Zip = 765432,
+                phone = 1226187458
+            });
+
+            contactList.Add(new Contact
+            {
+                Name = "haswik",
+                Address = "india",
+                city = "mumbai",
+                State = "mh",
+                email = "haswik@gmail.com",
+                Zip = 654321,
+                phone = 12261568
+            });
+            foreach (var contacts in contactList)
+            {
+                Console.WriteLine(contacts.Name);
+                Console.WriteLine(contacts.Address);
+                Console.WriteLine(contacts.city);
+                Console.WriteLine(contacts.State);
+                Console.WriteLine(contacts.email);
+                Console.WriteLine(contacts.Zip);
+                Console.WriteLine(contacts.phone);
+                Console.WriteLine("**************************");
+            }
+        }
+    }
+
+}
